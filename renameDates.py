8bd220fd912e28627c4c1d94fc63e004613706b8
Robert Reyes <reyesrobert96@yahoo.com>
2021-03-25 15:50:55 -0700

    adding chnages to rename

--- renameDates.py
+++ renameDates.py
@@ -3,9 +3,26 @@
 import shutil, os, re 
 
 # Creates a regex that matches fiels with the American date format
-datePatteren = re.compile(r"""^(.*?)# all text before the date
-    (( 0|1)?\d)                     # one or two digits for the month
+datePattern = re.compile(r"""^(.*?)# all text before the date
+    ((0|1)?\d)                      # one or two digits for the month
     ((0|1|2|3)?\d)-                 # one or two digits for the day 
     ((19|20)d\d)                    # four digits for the year
     (.*?)$                          # all text after the date
     """, re.VERBOSE) 
+
+# Loop over the files in the working directory
+for amerFilename in os.listdir('.'):
+    mo = datePattern.search(amerFilename)
+
+    #Skip files without a date
+    if mo == None: 
+        continue
+
+    # Get the different parts of the filename
+    beforePart = mo.group(1)
+    monthPart  = mo.group(2)
+    dayPart    = mo.group(4)
+    yearPart   = mo.group(6)
+    afterPart  = mo.group(8)
+
+
